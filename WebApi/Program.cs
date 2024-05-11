d0cae42cb9fd44c7e138274b720dd12005e32207
Yratilos <2197837920@qq.com>
2024-05-11 09:57:43 +0800

    配置修改:设置独立部署端口

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace WebApi
 {
@@ -10,11 +12,17 @@ namespace WebApi
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var Configuration = new ConfigurationBuilder().SetBasePath(Environment.CurrentDirectory)
+                                          .AddJsonFile("appsettings.json")
+                                          .Build();
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseConfiguration(Configuration);
                     webBuilder.UseStartup<Startup>();
                 });
+        }
     }
 }
