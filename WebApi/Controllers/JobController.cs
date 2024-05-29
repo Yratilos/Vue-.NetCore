20113a0398224ac50f7afe0c94d621282d6f29a5
Yratilos <2197837920@qq.com>
2024-05-29 11:07:38 +0800

    更新:请求成功直接返回表数据

--- WebApi/Controllers/JobController.cs
+++ WebApi/Controllers/JobController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Dtos;
 using WebApi.IServices;
+using WebApi.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,16 +19,14 @@ namespace WebApi.Controllers
             this.jobService = jobService;
         }
         [HttpPost]
-        public JobDto Log([FromBody] JobDto jobDto)
+        public Job Log([FromBody] JobDto jobDto)
         {
-            jobService.Log(jobDto);
-            return jobDto;
+            return jobService.Log(jobDto);
         }
         [HttpPost]
-        public JobDto Share([FromBody] JobDto jobDto)
+        public Job Share([FromBody] JobDto jobDto)
         {
-            jobService.Share(jobDto);
-            return jobDto;
+            return jobService.Share(jobDto);
         }
     }
 }
